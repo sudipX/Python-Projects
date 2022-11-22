2d7dcb267690ba09ab9b4b735261d887b28bc0da
Sudip Poudel <107059003+sudipX@users.noreply.github.com>
2022-11-22 08:04:28 +0545

    Add files via upload

---
+++ 001_Band_Name_Generator/main.py
@@ -0,0 +1,6 @@
+print("###### PYTHON BAND-NAME GENERATOR #######")
+
+city_name = input("What's the name of the city you grew up in ?\n")
+pet_name = input("What is your favourite pet ?\n")
+color_name = input("Enter your favourite color : \n")
+print("Your Band-Name Could be :\n" + color_name.capitalize() + pet_name.capitalize() + city_name.capitalize())
