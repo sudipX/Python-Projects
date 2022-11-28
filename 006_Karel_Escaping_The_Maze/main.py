ec790b2ccc7efdb65dd6282724d43ef99a0a8d88
Sudip Poudel <107059003+sudipX@users.noreply.github.com>
2022-11-28 22:26:56 +0545

    Add files via upload

---
+++ 006_Karel_Escaping_The_Maze/main.py
@@ -0,0 +1,24 @@
+# Escaping the maze
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif wall_on_right():
+#         turn_left()
+#     else:
+#         move()
