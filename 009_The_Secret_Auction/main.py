ed10413b2ab5e81594c469363634f1bf5167965f
Sudip Poudel <sudippoudel@sd.taylors.edu.my>
2022-12-20 20:42:25 +0545

    Code Completion

--- 009_The_Secret_Auction/main.py
+++ 009_The_Secret_Auction/main.py
@@ -15,8 +15,20 @@ while remaining_bidder:
     bidders_information.append(temp)
 
     any_other_bidder = input("Are there any other bidder ? Y / N\n>> ").lower()
-    if any_other_bidder == "n":
+    if any_other_bidder == "y":
+        os.system("cls")
+        print(logo)
+        print(text)
+    elif any_other_bidder == "n":
         remaining_bidder = False
 
-# finding the highest bidder from the bidders information list
-print(bidders_information)
+highest_bid_amount = 0
+highest_bidder_name = ""
+for items in bidders_information:
+    amount = items["Amount"]
+    if amount > highest_bid_amount:
+        highest_bid_amount = amount
+        highest_bidder_name = items["Name"]
+
+print("Analyzing results...............................")
+print(f"Highest bidder : {highest_bidder_name}\nBid Amount : ${int(highest_bid_amount)}")
