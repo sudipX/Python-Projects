1ff9da845e3cbfce661afcbc9a7e5aaa790fece6
sudipX <sudippoudel@sd.taylors.edu.my>
2022-12-30 23:36:19 +0545

    Fri 30 Dec, 23:36

--- 010_Calculator/main.py
+++ 010_Calculator/main.py
@@ -2,14 +2,7 @@ from resources import logo
 import os
 
 
-operations = {
-    "+" : "Addition",
-    "-" : "Subtraction",
-    "*" : "Multiplication",
-    "/" : "Division"
-}
-
-def addtion(num1, num2):
+def addition(num1, num2):
     return num1 + num2
 
 def subtraction(num1, num2):
@@ -21,35 +14,35 @@ def multiplication(num1, num2):
 def division(num1, num2):
     return num1 / num2
 
+operations = {
+    "+" : addition,
+    "-" : subtraction,
+    "*" : multiplication,
+    "/" : division
+}
 
-continue_calculation = True
-
-while continue_calculation:
+def calculator():
     print(logo)
     first_num = int(input("Enter the number:\n>> "))
-    for items in operations:
-        print(f"{items} : {operations[items]}")
-    operator = input("Choose your operator:\n>> ")
-    second_num = int(input("Enter the number:\n>> "))
+    continue_calculation = True
+    while continue_calculation:
+        for items in operations:
+            print(f"{items} : {operations[items]}")
+        operator = input("Choose your operator:\n>> ")
+        second_num = int(input("Enter next number:\n>> "))
 
-    def calculation(num1, operator_input, num2):
-        if operator_input == "+":
-            output = addtion(num1, num2)
-            return output
-        elif operator_input == "-":
-            output = subtraction(num1, num2)
-            return output
-        elif operator_input == "*":
-            output = multiplication(num1, num2)
-            return output
-        elif operator_input == "/":
-            output = division(num1, num2)
-            return output
+        calculation = operations[operator]
+        output = calculation(num1=first_num, num2=second_num)
 
-    output = calculation(num1=first_num, operator_input=operator, num2=second_num)
-    print(f"Your Output:\n{first_num} {operator} {second_num}  = {output}\n------------------------------------------------------------------")
+        print(f"Your Output:\n{first_num} {operator} {second_num}  = {output}\n------------------------------------------------------------------")
+        user_input = input(f"Type 'Y' to continue calculation with {output} or 'N' to start new calculation & 'E' to exit:\n>> ").lower()
+        if user_input == "y":
+            first_num = output
+        elif user_input == "n":
+            os.system("clear")
+            calculator()
+        elif user_input == "e":
+            print("GOODBYE !!!")
+            break
 
-    user_option = input("a. Continue Calculation\nb. New Calculation\n>> ")
-    if user_option == "b":
-        os.system("clear")
-
+calculator()
